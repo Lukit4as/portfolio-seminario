43255fb09d298ebca976e26df3bdd51c9af0d3f9
Lukit4as <lucassuarez873@gmail.com>
2026-09-19 01:32:27 +0000

    Preparar app de Gradio para deploy

--- Clase_04/app.py
+++ Clase_04/app.py
@@ -1,5 +1,5 @@
 import gradio as gr
-
+import os
 
 def saludar(nombre):
     return f"¡Hola, {nombre}! Bienvenido a mi portfolio de Seminario."
@@ -20,4 +20,4 @@ with gr.Blocks() as app:
     )
 
 
-app.launch()
+app.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
